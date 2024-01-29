988b031d523dbef3f999d2d6e4ad7815dbfaeefd
Bomi Cholidiza <BCholidiza@oldmutual.com>
2024-01-29 09:36:54 +0200

    Routing updated

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,11 +1,20 @@
 import { Button, Card, Image } from "semantic-ui-react";
 import useStore from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import { Link, useParams } from "react-router-dom";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useEffect } from "react";
 
 const ActivityDetails = () => {
 	const { activityStore } = useStore();
-	const { selectedActivity: activity, openForm, cancelSelectedActivity } = activityStore;
+	const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore;
+	const { id } = useParams();
 
-	if (!activity) return;
+	useEffect(() => {
+		if (id) loadActivity(id);
+	}, [id, loadActivity]);
+
+	if (loadingInitial || !activity) return <LoadingComponent />;
 
 	return (
 		<Card fluid>
@@ -19,14 +28,12 @@ const ActivityDetails = () => {
 			</Card.Content>
 			<Card.Content extra>
 				<Button.Group widths="2">
-					<Button onClick={() => openForm(activity.id)} basic color="blue" content="Edit" />
-
-					{/** No need to wrap in arrow function because it does not pass a parameter */}
-					<Button onClick={cancelSelectedActivity} basic color="grey" content="Cancel" />
+					<Button as={Link} to={`/manage/${activity.id}`} basic color="blue" content="Edit" />
+					<Button as={Link} to={`/activities}`} basic color="grey" content="Cancel" />
 				</Button.Group>
 			</Card.Content>
 		</Card>
 	);
 };
 
-export default ActivityDetails;
+export default observer(ActivityDetails);
