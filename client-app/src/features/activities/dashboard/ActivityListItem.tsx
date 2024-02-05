3ed455862dd8fd06ab2cf5d318a7d3837788c19a
Bomi Cholidiza <BCholidiza@oldmutual.com>
2024-02-05 09:01:44 +0200

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,26 +1,12 @@
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
-import useStore from "../../../app/stores/store";
-import { SyntheticEvent, useState } from "react";
 
 interface Props {
 	activity: Activity;
 }
 
 const ActivityListItem = ({ activity }: Props) => {
-	const { activityStore } = useStore();
-	const { deleteActivity, loading } = activityStore;
-
-	const [target, setTarget] = useState("");
-
-	// use this function to ensure that the correct button is targetted for deletion
-	// if this function is not there, all buttons show loader on deletion
-	const handleActivityDelete = (e: SyntheticEvent<HTMLButtonElement>, id: string) => {
-		setTarget(e.currentTarget.name);
-		deleteActivity(id);
-	};
-
 	return (
 		<Segment.Group>
 			<Segment>
